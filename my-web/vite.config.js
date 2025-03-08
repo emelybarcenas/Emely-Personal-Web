de006607a473d58e3dc31ed6984e74854c4bb5d1
emelybarcenas <emelyxbarcenas@gmail.com>
2025-03-08 13:18:53 -0500

    changes to try deployment

--- my-web/vite.config.js
+++ my-web/vite.config.js
@@ -5,7 +5,6 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: "./", // Ensures assets load correctly
   css: {
     postcss: "./postcss.config.js",
   },
