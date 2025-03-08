17b2513b78ed130f7dad51d5b298a45ebf50b62e
emelybarcenas <emelyxbarcenas@gmail.com>
2025-03-08 14:15:12 -0500

    workign deployment w/ responsive updates

--- my-web/vite.config.js
+++ my-web/vite.config.js
@@ -1,10 +1,9 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
-// https://vite.dev/config/
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
+  base: "/", // Ensures correct asset paths for Vercel
   css: {
     postcss: "./postcss.config.js",
   },
